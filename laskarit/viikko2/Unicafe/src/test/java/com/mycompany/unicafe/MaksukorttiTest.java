640f4dc675ae05c34649dcb716ccffa21b1c4bc3
Jaakko Paavola <jaakko.paavola@helsinki.fi>
2019-11-12 19:40:21 +0200

    Viikon 2 tehtavat.

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,29 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+
+    @Test
+    public void kortinSaldoAlussaOikein() {
+        assertEquals("saldo: " + 10/100 + "." + 10%100, kortti.toString());
+    }
+
+    @Test
+    public void rahanLataaminenKasvattaaSaldoaOikein() {
+        kortti.lataaRahaa(90);
+        assertEquals("saldo: " + 100/100 + "." + 100%100, kortti.toString());
+    }
+
+    @Test
+    public void rahanOttaminenToimii() {
+        assertTrue(kortti.otaRahaa(1));
+        assertEquals("saldo: " + 9/100 + "." + 9%100, kortti.toString());
+        assertFalse(kortti.otaRahaa(11));
+        assertEquals("saldo: " + 9/100 + "." + 9%100, kortti.toString());
+    }
+
+    @Test
+    public void saldoPalauttaaSaldon() {
+        assertEquals(10, kortti.saldo());
+    }
+
 }
